470aca5f053c78a1d064f90884d44b7259503390
Roshan Ghadge <roshanghadge568@gmail.com>
2026-08-27 17:47:35 +0530

    Added practice set of code to working with getbyrole

--- pages/PlaywrightPage.ts
+++ pages/PlaywrightPage.ts
@@ -8,6 +8,9 @@ export class PlaywrightPage extends BasePage {
     private readonly divButtonRole;
     private readonly userNameTextField;
     private readonly checkboxField;
+    private readonly homeLink;
+    private readonly productsLink;
+    private readonly contactLink;
 
     constructor(page: Page) {
         super(page);
@@ -17,23 +20,34 @@ export class PlaywrightPage extends BasePage {
         this.divButtonRole = this.page.getByRole('button', {
             name: 'Div with button role'
         });
-        this.userNameTextField = this.page.getByRole('textbox').filter({ has: this.page.locator('input#username') });
-        this.checkboxField = this.page.getByRole('checkbox', { name: 'Accept terms' });
-
-
+        this.userNameTextField = this.page.locator('input#username');
+        this.checkboxField = this.page.locator("//input[@type='checkbox']")
+        this.homeLink = this.page.locator('#role-locators').getByRole('link', { name: 'Home' });
+        this.productsLink = this.page.locator('#role-locators').getByRole('link', { name: 'Products' });
+        this.contactLink = this.page.locator('#role-locators').getByRole('link', { name: 'Contact' });
     }
 
 
     async testgetByRole() {
+        await this.page.waitForLoadState('domcontentloaded');
         await expect(this.primaryActionButton).toBeVisible({ timeout: 5000 });
         await this.primaryActionButton.click({ timeout: 5000 });
         await expect(this.toggleButton).toBeVisible({ timeout: 5000 });
         await this.toggleButton.click({ timeout: 5000 });
         await expect(this.divButtonRole).toBeVisible({ timeout: 5000 });
         await this.divButtonRole.click({ timeout: 5000 });
-        await expect(this.userNameTextField).toBeEnabled({ timeout: 5000 });
-        await this.userNameTextField.type('Roshan Ghadge', { delay: 500 });
-        await expect(this.checkboxField).toBeVisible({ timeout: 5000 });
+        await expect(this.userNameTextField).toBeVisible({ timeout: 5000 });
+        await this.userNameTextField.fill('Roshan Ghadge');
+        // await expect(this.checkboxField).toBeVisible({ timeout: 5000 });
         await this.checkboxField.click({ timeout: 5000 });
+        await expect(this.homeLink).toBeVisible();
+        await this.homeLink.click();
+        await this.page.goBack();
+        await expect(this.productsLink).toBeVisible();
+        await this.productsLink.click();
+        await this.page.goBack();
+        await expect(this.contactLink).toBeVisible();
+        await this.contactLink.click();
+        await this.page.goBack();
     }
 }
