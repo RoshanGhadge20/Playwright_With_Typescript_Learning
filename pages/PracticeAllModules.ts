96a93c847e76d3f546a925005f830d4d4d04d846
Roshan Ghadge <roshanghadge568@gmail.com>
2026-09-07 15:46:01 +0530

    Practice set with the dialog and alert handling

--- pages/PracticeAllModules.ts
+++ pages/PracticeAllModules.ts
@@ -7,9 +7,12 @@ export class PracticeAllModules extends BasePage {
     private readonly nameField;
     private readonly countryDropdown;
 
+    private readonly confirmationAlert;
+    private readonly promptDialog;
 
+    private readonly newTab;
 
-
+    private readonly downlaodSection;
 
 
 
@@ -18,6 +21,10 @@ export class PracticeAllModules extends BasePage {
         // this.page = page;
         this.nameField = this.page.getByPlaceholder('Enter Name');
         this.countryDropdown = this.page.locator('select#country');
+        this.confirmationAlert = this.page.getByText('Confirmation Alert');
+        this.promptDialog = this.page.getByText('Prompt Alert');
+        this.newTab = this.page.getByText('New Tab');
+        this.downlaodSection = this.page.getByText('Downlaod Button');
     }
 
 
@@ -27,7 +34,49 @@ export class PracticeAllModules extends BasePage {
     }
 
     async handlingDialogAlerts() {
-
+        this.page.once('dialog', async (dialog) => {
+            console.log(await dialog.type() === 'confirm');
+            console.log(await dialog.accept());
+        }
+        );
+        await this.confirmationAlert.click();
     }
+
+    async handlingPromptDialog() {
+        this.page.on('dialog', async (dialog) => {
+            console.log(await dialog.type());
+            if (dialog.type() === 'prompt') {
+                await dialog.accept("My name is roshan Ghadge");
+            }
+        });
+        await this.promptDialog.click();
+    }
+
+    async handlingNewTabSection() {
+        const [newTab] = await Promise.all(
+            [
+                console.log('Hello'),
+                await this.page.context().waitForEvent('page'),
+                this.newTab.click()
+            ]
+        );
+        // await newTab.waitForLoadState();
+        console.log('Handled the new tab');
+        // await newTab.close();
+        await this.page.bringToFront();
+    }
+
+    async handlingDownloadSection() {
+        const [downloadFile] = await Promise.all([
+            await this.page.waitForEvent('download'),
+            await this.downlaodSection.click(),
+        ]);
+
+        // await this.downlaodSection.waitFor();
+        console.log(downloadFile.suggestedFilename());
+        const path = await downloadFile.path();
+        await downloadFile.saveAs('test/DownloadedFileSave.pdf')
+    }
+
 
 }
--- tests/ui/PracticeAllModule.spec.ts
+++ tests/ui/PracticeAllModule.spec.ts
@@ -7,5 +7,21 @@ test.describe("Practicing all individual of modules in playwright", async () => 
 
     test("Practicing the common actions", async ({ practiceAllModules }) => {
         await practiceAllModules.PerformCommonActions();
-    })
+    });
+
+    test("Practicing the confirmation dialog", async ({ practiceAllModules }) => {
+        await practiceAllModules.handlingDialogAlerts();
+    });
+
+    test("Practicing the prompt dialog", async ({ practiceAllModules }) => {
+        await practiceAllModules.handlingPromptDialog();
+    });
+
+    test("Practicing the new tab  section", async ({ practiceAllModules }) => {
+        await practiceAllModules.handlingNewTabSection();
+    });
+
+    test("Practicing the download section", async ({ practiceAllModules }) => {
+        await practiceAllModules.handlingDownloadSection();
+    });
 });
